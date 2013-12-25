fb1d7d7d326f5ca2fa43b304f48a128844fec2e0
Ben Phelps <bphelpsen@gmail.com>
2013-12-25 13:14:04 -0600

    init

---
+++ rotation.lua
@@ -0,0 +1,19 @@
+-- ProbablyEngine Rotation Packager
+-- Custom Arcane Mage Rotation
+-- Created on Nov 12th 2013 12:59 am
+ProbablyEngine.rotation.register_custom(62, "ProbabyTest", {
+
+{ "Ice Floes", {
+  "!player.buff(Ice Floes)",
+  "!modifier.last(Ice Floes)",
+  "player.moving"
+}},
+
+{ "Arcane Blast" },
+
+{ "Ice Lance" },
+
+
+}, function()
+  print('yes')
+end)
